03bf5b87012dbd20bdf0344e269e70da19c74f1a
Pratiklanjewar2025 <pratiklanjewar22@gmail.com>
2025-03-31 11:25:05 +0530

    Update main.tf

--- auto_scaling_project/terraform/main.tf
+++ auto_scaling_project/terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "autoscale_server" {
-  ami = "ami-0c55b159cbfafe1f0"
+  ami = "ami-084568db4383264d4"
   instance_type = "t2.micro"
   key_name = "pratik-linux-key1"
   security_groups = ["sg-0a06f350d00ce013c"]
