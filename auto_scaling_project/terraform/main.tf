57fbce58d9c8fbf644d5010ce5c565ff4046ea99
Pratiklanjewar2025 <pratiklanjewar22@gmail.com>
2025-03-31 11:22:24 +0530

    Update main.tf

--- auto_scaling_project/terraform/main.tf
+++ auto_scaling_project/terraform/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "autoscaling_instance" {
-  ami           = "ami-12345678"  # Replace with latest Ubuntu AMI ID
+resource "aws_instance" "autoscale_server" {
+  ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  key_name      = "your-key"
-  
-  user_data = file("userdata.sh")
+  key_name = "pratik-linux-key1"
+  security_groups = ["sg-0a06f350d00ce013c"]
+  user_data = file("${path.module}/userdata.sh")
 
   tags = {
-    Name = "AutoScalingInstance"
+    Name = "AutoScalingServer"
   }
 }
